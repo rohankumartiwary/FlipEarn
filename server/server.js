54b473432ec79c879bddc952c7aad9a6765d59ae
Rohan <rohan.kumar@appvintech.com>
2026-02-27 11:46:17 +0530

    Added

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 
 const app=express();
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use(clerkMiddleware());
 app.use(express.json());
 app.use(cors());
@@ -13,7 +14,6 @@ app.use(cors());
 app.get("/",(req,res)=>{
     res.send("Server is running");
 })
-app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
 
