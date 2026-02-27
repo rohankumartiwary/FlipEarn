5450828f49d1a170b26198bd6b4706b643819772
Rohan <rohan.kumar@appvintech.com>
2026-02-27 11:55:56 +0530

    added

--- server/config/prisma.js
+++ server/config/prisma.js
@@ -19,6 +19,6 @@ const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaNeon({ connectionString });
 const prisma = global.prisma || new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+if (process.env.NODE_ENV === 'production') global.prisma = prisma;
 
 export default prisma;
