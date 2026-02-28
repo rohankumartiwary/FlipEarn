5fa738d4e215ddcac6173321482d9620ce38a3c7
Rohan Kumar Tiwary <132188663+Rohan-tiwary@users.noreply.github.com>
2026-02-28 21:59:55 +0530

    Added

--- server/server.js
+++ server/server.js
@@ -1,26 +1,15 @@
-import express from "express";
-import "dotenv/config";
-import cors from "cors";
-import { clerkMiddleware } from "@clerk/express";
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js";
+import { inngest, functions } from "./inngest/index.js"
 
-const app = express();
+const app=express();
 app.use(clerkMiddleware());
 app.use(express.json());
 app.use(cors());
 
-<<<<<<< HEAD
-app.get("/", (req, res) => {
-  res.send("Hey");
-});
-app.use("/api/inngest", serve({ client: inngest, functions }));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
-=======
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
@@ -34,4 +23,3 @@ const PORT=process.env.PORT || 3000
 app.listen(PORT,()=>{
     console.log("Server running");
 })
->>>>>>> a9c622eb6608da6066f59cb0ab3dd5a8cbbb1d12
